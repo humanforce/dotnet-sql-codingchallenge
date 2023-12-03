e4d6afdc620d70a7450de927455e94e50d1822a2
charlesflorido <charlesflorido@outlook.com>
2023-12-04 04:21:49 +0800

    WebApi - add sales-page module to routes

--- carsales-webapp/src/app/app.routes.ts
+++ carsales-webapp/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'sales',
+        loadChildren: () => import('./pages/sales-page/sales.page.module').then(m => m.SalesPageModule)
+    },
+    {
+        path: '',
+        redirectTo: 'sales',
+        pathMatch: 'full'
+    }
+];
