df799394e0b0f1412e882bb18530d7e509bfa3e8
charlesflorido <charlesflorido@outlook.com>
2023-12-04 05:07:38 +0800

    WebApp - updated SalesPage

--- carsales-webapp/src/app/pages/sales-page/sales.page.ts
+++ carsales-webapp/src/app/pages/sales-page/sales.page.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core'
+import { FormControl, FormGroup } from '@angular/forms'
+import { CarSale, ICarSalesClient } from '../../../shared/carsales-api';
 
 @Component({
     selector: 'sales-page',
@@ -7,5 +9,28 @@ import {Component, OnInit} from '@angular/core'
 })
 export class SalesPage
 {
-    
+    public searchForm: FormGroup;
+    public sales: Array<CarSale> = [];
+
+    constructor(private carSalesClient: ICarSalesClient)
+    {
+        this.searchForm = new FormGroup({
+            startDate: new FormControl(),
+            endDate: new FormControl()
+        });
+    }
+
+    onSubmit() {
+        var startDate = this.searchForm.get('startDate')?.value;
+        var endDate = this.searchForm.get('endDate')?.value;
+        console.log(`startDate: ${startDate} | ${typeof(startDate)}`);
+        console.log(`endDate: ${endDate} | ${typeof(endDate)}`);
+
+        this.carSalesClient.getSales(
+            typeof(startDate) === 'string' ? new Date(startDate) : null,
+            typeof(endDate) === 'string' ? new Date(endDate) : null
+        ).subscribe((data)=> {
+            this.sales = data;
+        })
+    }
 }
