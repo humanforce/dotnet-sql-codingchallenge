f1732972869f674b4c1ab8772a1bca58cd1ded08
charlesflorido <charlesflorido@outlook.com>
2023-12-04 16:31:09 +0800

    WebApi - add custom contract resolver

---
+++ CarSales.WebApi/src/CarSales.WebApi/Configuration/CarApiJsonContractResolver.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection;
+
+namespace CarSales.WebApi
+{
+    public class CarApiJsonContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            JsonProperty property = base.CreateProperty(member, memberSerialization);
+
+            if (member.GetCustomAttribute<InversePropertyAttribute>() != null)
+            {
+                property.ShouldSerialize =
+                    instance => false;
+            }
+
+            return property;
+        }
+
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName))
+                return string.Empty;
+
+            return $"{char.ToLower(propertyName[0])}{propertyName.Substring(1)}";
+        }
+    }
+}
--- CarSales.WebApi/src/CarSales.WebApi/Program.cs
+++ CarSales.WebApi/src/CarSales.WebApi/Program.cs
@@ -13,7 +13,11 @@ public class Program
 
         // Add services to the container.
 
-        builder.Services.AddControllers();
+        builder.Services.AddControllers().AddNewtonsoftJson(options =>
+        {
+            options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            options.SerializerSettings.ContractResolver = new CarApiJsonContractResolver();
+        });
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
